de926455f0ef8f2fa160fa59c3bdaab75dda6efa
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2025-04-16 19:57:09 +0200

    Moved infinite loops to separate threads

--- app/src/main/java/org/example/chat/ChatServer.java
+++ app/src/main/java/org/example/chat/ChatServer.java
@@ -8,23 +8,25 @@ public class ChatServer {
     private ChatServer() {}
 
     public static void startServer(int port){
-        try(ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.printf("[SERVER STARTED ON PORT %d]%n", port);
+        new Thread(() -> {
+            try(ServerSocket serverSocket = new ServerSocket(port)) {
+                System.out.printf("[SERVER STARTED ON PORT %d]%n", port);
 
-            while(serverSocket.isBound()) {
-                Socket socket = serverSocket.accept();
-                try {
-                    new ChatClientHandler(socket);
+                while(serverSocket.isBound()) {
+                    Socket socket = serverSocket.accept();
+                    try {
+                        new ChatClientHandler(socket);
+                    }
+                    catch (IOException | ClassNotFoundException e){
+                        System.out.println("SERVER ERROR[Can't create ChatClientHandler!]");
+                        e.printStackTrace();
+                        socket.close();
+                    }
                 }
-                catch (IOException | ClassNotFoundException e){
-                    System.out.println("SERVER ERROR[Can't create ChatClientHandler!]");
-                    e.printStackTrace();
-                    socket.close();
-                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+        }).start();
     }
 }
